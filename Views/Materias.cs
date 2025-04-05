1608a11012a2c0951f4a1501e80607eb134a6f1f
erick21340 <Torot0086@gmail.com>
2025-04-05 13:35:15 -0900

    realizando la pantalla de materia

---
+++ Views/Materias.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ucne_guia_desktop.Controllers;
+
+namespace ucne_guia_desktop.Views
+{
+    public partial class Materias : Form
+    {
+        public Materias()
+        {
+            InitializeComponent();
+        }
+
+        private async void Materia_Load(object sender, EventArgs e)
+        {
+            MateriaController materia = new MateriaController();
+            materiadatagri.DataSource = await materia.getmateria();
+        }
+    }
+}
