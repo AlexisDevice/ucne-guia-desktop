2611bd7888d8fa9d15483fe200c28c47e467b916
erick21340 <Torot0086@gmail.com>
2025-04-05 18:19:58 -0900

    agregando  carrera

---
+++ Views/Carreras.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ucne_guia_desktop.Controllers;
+
+namespace ucne_guia_desktop.Views
+{
+    public partial class Carreras : Form
+    {
+        public Carreras()
+        {
+            InitializeComponent();
+        }
+
+        private async void Carreras_Load(object sender, EventArgs e)
+        {
+            CarreraController carrera = new CarreraController();
+            Carreragril.DataSource = carrera.GetCarreras;
+        }
+    }
+}
